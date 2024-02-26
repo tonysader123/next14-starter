f91b7ae7f3fda49f7b4eb8a7a54fa6fb59e299a7
Tony <tony.sader@live.com>
2024-02-25 20:36:01 -0500

    setup pages

---
+++ src/components/NavBar/navbar.jsx
@@ -0,0 +1,14 @@
+import Links from "./links/links"
+
+const NavBar =() => {
+    return (
+        <div>
+            <div>Logo</div>
+            <div>
+                <Links />
+            </div>    
+        </div>
+    )
+}
+
+export default NavBar;
